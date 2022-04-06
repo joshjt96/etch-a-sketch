cbf6991b89d24935eae3106a574848c47838d603
Josh Talley <jtdev84@gmail.com>
2022-04-06 17:59:18 +0100

    Create makeGrid function to display a 16*16 grid of square divs

--- etch.js
+++ etch.js
@@ -1,20 +1,11 @@
 const container = document.querySelector('.container');
 
-function makeRow() {
-    for (let i = 0; i < 16; i++) {
+function makeGrid() {
+    for (let i = 0; i < 256; i++) {
         const div = document.createElement('div');
         div.classList.add('grid-box');
-        container.after(div)
+        container.appendChild(div);
     };
 };
 
-// function makeColumn() {
-//     for (let i = 0; i < 16; i++) {
-//         const div = document.createElement('div');
-//         div.classList.add('grid-box');
-//         container.after(div)
-//     }
-// }
-
-makeRow();
-makeColumn();
+makeGrid();
